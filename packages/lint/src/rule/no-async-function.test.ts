a0490821b86339a202a3d2214570bfe7c4cf9fee
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-07-04 03:16:48 +0900

    feat: add no-async-function

---
+++ packages/lint/src/rule/no-async-function.test.ts
@@ -0,0 +1,45 @@
+import { RuleTester } from "@typescript-eslint/rule-tester";
+import rule from "./no-async-function";
+import { it, describe } from "vite-plus/test";
+RuleTester.afterAll = () => {};
+RuleTester.it = it;
+RuleTester.itOnly = it.only;
+RuleTester.itSkip = it.skip;
+RuleTester.describe = describe;
+RuleTester.describeSkip = describe.skip;
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-async-function", rule, {
+  valid: [
+    {
+      code: `() => {}`,
+    },
+    {
+      code: `function a(){}`,
+    },
+    {
+      code: `function* a(){}`,
+    },
+    {
+      code: `const a = function(){}`,
+    },
+  ],
+  invalid: [
+    {
+      code: `async () => {}`,
+      errors: [{ messageId: "nonUseAsyncArrowFunction" }],
+    },
+    {
+      code: `async function a(){}`,
+      errors: [{ messageId: "nonUseAsyncFunction" }],
+    },
+    {
+      code: `async function* a(){}`,
+      errors: [{ messageId: "nonUseAsyncGeneratorFunction" }],
+    },
+    {
+      code: `const a = async function(){}`,
+      errors: [{ messageId: "nonUseAsyncFunction" }],
+    },
+  ],
+});
---
+++ packages/lint/src/rule/no-async-function.ts
@@ -0,0 +1,79 @@
+// oxlint-disable-next-line no-unused-vars
+import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/utils";
+import type { RuleModule } from "@typescript-eslint/utils/eslint-utils";
+export default {
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Disallow the use of async keyword",
+    },
+    messages: {
+      nonUseAsyncArrowFunction:
+        "Do not use async arrow function. use normal arrow function instead",
+      nonUseAsyncFunction:
+        "Do not use async function. use normal function instead",
+      nonUseAsyncGeneratorFunction:
+        "Do not use async generator function. use normal generator function instead",
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      FunctionDeclaration(node) {
+        if (node.async) {
+          if (node.generator) {
+            context.report({
+              messageId: "nonUseAsyncGeneratorFunction",
+              node,
+              fix(fixer) {
+                const asyncToken = context.sourceCode.getFirstToken(node)!;
+                const nextToken = context.sourceCode.getTokenAfter(asyncToken)!;
+
+                return fixer.replaceTextRange(
+                  [asyncToken.range[0], nextToken.range[0]],
+                  "",
+                );
+              },
+            });
+          } else {
+            context.report({
+              messageId: "nonUseAsyncGeneratorFunction",
+              node,
+              fix(fixer) {
+                const asyncToken = context.sourceCode.getFirstToken(node)!;
+                const nextToken = context.sourceCode.getTokenAfter(asyncToken)!;
+
+                return fixer.replaceTextRange(
+                  [asyncToken.range[0], nextToken.range[0]],
+                  "",
+                );
+              },
+            });
+          }
+        }
+      },
+      ArrowFunctionExpression(node) {
+        if (node.async) {
+          context.report({
+            messageId: "nonUseAsyncArrowFunction",
+            node,
+            fix(fixer) {
+              const asyncToken = context.sourceCode.getFirstToken(node)!;
+              const nextToken = context.sourceCode.getTokenAfter(asyncToken)!;
+
+              return fixer.replaceTextRange(
+                [asyncToken.range[0], nextToken.range[0]],
+                "",
+              );
+            },
+          });
+        }
+      },
+    };
+  },
+} as const satisfies RuleModule<
+  | "nonUseAsyncFunction"
+  | "nonUseAsyncArrowFunction"
+  | "nonUseAsyncGeneratorFunction"
+>;
