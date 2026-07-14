7e26e6063cd26b8df60355a1280a3e984b156166
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-07-14 19:54:42 +0900

    feat: add tests

--- packages/build/src/builder/utils.test.ts
+++ packages/build/src/builder/utils.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vite-plus/test";
-import { getNameFromIdentifierOrStringLiteral, resolvePath } from "./utils";
+import { generateGlobalThisModuleMemberExpression, getModuleKeyOrThrow, getNameFromIdentifierOrStringLiteral, isNodePackage, resolvePath } from "./utils";
 import { identifier, stringLiteral } from "@babel/types";
-
+import {generate} from "@babel/generator"
 describe("test resolvePath", () => {
   it("should return resolved path", () => {
     expect(resolvePath("./b", "src/a")).toBe("src/b");
@@ -21,3 +21,28 @@ describe("test getNameFromIdentifierOrStringLiteral", () => {
     );
   });
 });
+
+describe("test getModuleKeyOrThrow",() => {
+  it("when input path is exists,return value",() => {
+    expect(getModuleKeyOrThrow(new Map([["name","1"]]),"name")).toBe("1")
+  })
+    it("when input path is not exists, throw",() => {
+    expect(() => getModuleKeyOrThrow(new Map([["name","1"]]),"non_exists_name")).throw()
+  })
+})
+
+describe("test isNodepackage",() => {
+  it("when input node package name,return true",() => {
+    expect(isNodePackage("typescript")).toBe(true)
+    expect(isNodePackage("@bloxdjs/build")).toBe(true)
+  })
+    it("when input file path,return false",() => {
+    expect(isNodePackage("./foo.ts")).toBe(false)
+    expect(isNodePackage("../foo/bar.tsx")).toBe(false)
+  })
+})
+describe("test generateGlobalThisModuleMemberExpression",() => {
+  it("return globalThis member expression",() => {
+    expect(generate(generateGlobalThisModuleMemberExpression("1")).code).toBe('globalThis.__b_m__["1"]')
+  })
+})
